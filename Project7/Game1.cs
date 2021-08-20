df60095960650f264dbcb60663968c0d43cdf9ea
Prikhod322 <vovkaprikhod@gmail.com>
2021-08-20 22:51:55 +0300

    Added buildings menu

--- Project7/Game1.cs
+++ Project7/Game1.cs
@@ -40,6 +40,25 @@ namespace Project7
         }
     }
 
+    public class SpriteBuilding
+    {
+        public Building BuildingType;
+        public Rectangle SpriteRectangle;
+        public Texture2D[] Texture;
+        public Texture2D[] Icon;
+        public string[] IconPath;
+        public string[] TexturePath;
+
+        public SpriteBuilding(Building buildingType, Rectangle spriteRectangle, Texture2D[] texture, Texture2D[] icon, string[] iconPath, string[] texturePath)
+        {
+            BuildingType = buildingType;
+            SpriteRectangle = spriteRectangle;
+            Texture = texture;
+            Icon = icon;
+            IconPath = iconPath;
+            TexturePath = texturePath;
+        }
+    }
     
     public class Game1 : Game
     {
@@ -51,6 +70,8 @@ namespace Project7
 
         List<SpriteResourse> Resourse = new List<SpriteResourse>();
         List<SpriteFont> ResourseAmount = new List<SpriteFont>();
+
+        List<SpriteBuilding> Building = new List<SpriteBuilding>();
         
         Texture2D wf;
         Texture2D wf1;
@@ -69,12 +90,13 @@ namespace Project7
         Sprite Up;
         Sprite Down;
 
-        Texture2D tx;
-        Texture2D col;
         SpriteFont font;
 
         SpriteFont UnsafeRes;
         SpriteFont UnsafeGold;
+
+        //SpriteFont Green;
+        //SpriteFont Red;
 
         int IntUnsafeRes;
         int IntUnsafeGold;
@@ -102,6 +124,8 @@ namespace Project7
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
+            //Green = Content.Load<SpriteFont>("Font/Green");
+           // Red = Content.Load<SpriteFont>("Font/DefaultFont");
             font = Content.Load<SpriteFont>("Font/DefaultFont");
             UnsafeRes = Content.Load<SpriteFont>("Font/DefaultFont");
             UnsafeGold = Content.Load<SpriteFont>("Font/DefaultFont");
@@ -123,8 +147,7 @@ namespace Project7
 
             wf1 = Content.Load<Texture2D>("Textures/Animations/46.1");
             wf2 = Content.Load<Texture2D>("Textures/Animations/46.2");
-            tx = Content.Load<Texture2D>("Textures/18");
-            col = Content.Load<Texture2D>("Textures/12");
+           
 
             Up.Texture = Content.Load<Texture2D>(Up.TexturePath);
             Down.Texture = Content.Load<Texture2D>(Down.TexturePath);
@@ -135,7 +158,7 @@ namespace Project7
             Background.Texture = Content.Load<Texture2D>(Background.TexturePath);
             BuildMenu.Texture = Content.Load<Texture2D>(BuildMenu.TexturePath);
             WorkMenu.Texture = Content.Load<Texture2D>(WorkMenu.TexturePath);
-            ResourseMenu.Texture = Content.Load<Texture2D>(ResourseMenu.TexturePath);
+            ResourseMenu.Texture = Content.Load<Texture2D>(ResourseMenu.TexturePath); 
 
             Resourse.Add(new SpriteResourse(new Rock(),       new Rectangle(100, 140, 80, 80), null, "Textures/stone"));
             Resourse.Add(new SpriteResourse(new IronOre(),    new Rectangle(400, 140, 80, 80), null, "Textures/iron ore"));
@@ -152,6 +175,22 @@ namespace Project7
             for (int i = 0; i < Resourse.Count; i++)
             {
                 Resourse[i].Texture = Content.Load<Texture2D>(Resourse[i].TexturePath);
+            }
+
+            Building.Add(new SpriteBuilding(new Sawmill(),    new Rectangle(150, 200, 80, 80), null, new Texture2D[] { null, null }, new string[] { "Textures/saw", "Textures/sawB" }, new string[] { null, null, null }));
+            Building.Add(new SpriteBuilding(new LumberJack(), new Rectangle(300, 200, 80, 80),null, new Texture2D[] { null, null },  new string[] { "Textures/lumberjack", "Textures/lumberjackB" }, new string[] { null, null, null }));
+            Building.Add(new SpriteBuilding(new Forge(),      new Rectangle(450, 200, 80, 80), null, new Texture2D[] { null, null }, new string[] { "Textures/anvil", "Textures/anvilB" }, new string[] { null, null, null }));
+            Building.Add(new SpriteBuilding(new Fisher(),     new Rectangle(150, 350, 80, 80), null, new Texture2D[] { null, null }, new string[] { "Textures/fish", "Textures/fishB" }, new string[] { null, null, null }));
+            Building.Add(new SpriteBuilding(new Collector(),  new Rectangle(300, 350, 80, 80), null, new Texture2D[] { null, null }, new string[] { "Textures/bag", "Textures/bagB" }, new string[] { null, null, null }));
+            Building.Add(new SpriteBuilding(new Kitchen(),    new Rectangle(450, 350, 80, 80), null, new Texture2D[] { null, null }, new string[] { "Textures/kitchen", "Textures/kitchenB" }, new string[] { null, null, null }));
+            Building.Add(new SpriteBuilding(new Town(),       new Rectangle(150, 500, 80, 80), null, new Texture2D []{ null, null }, new string[] { "Textures/bed", "Textures/bedB" }, new string[] { null, null, null }));
+            Building.Add(new SpriteBuilding(new Farmer(),     new Rectangle(300, 500, 80, 80), null, new Texture2D[] { null, null }, new string[] { "Textures/farm", "Textures/farmB" }, new string[] { null, null, null }));
+            Building.Add(new SpriteBuilding(new Miner(),      new Rectangle(450, 500, 80, 80), null, new Texture2D[] { null, null }, new string[] { "Textures/mine", "Textures/mineB" }, new string[] { null, null, null }));
+
+            for (int i = 0; i < Building.Count; i++)
+            {
+                Building[i].Icon[0] = Content.Load<Texture2D>(Building[i].IconPath[0]);
+                Building[i].Icon[1] = Content.Load<Texture2D>(Building[i].IconPath[1]);
             }
         }
 
@@ -199,7 +238,7 @@ namespace Project7
                         Selection2.SpriteRectangle = new Rectangle(Resourse[i].SpriteRectangle.X - 5, Resourse[i].SpriteRectangle.Y - 5, 90, 90);
                         if (Mouse.GetState().LeftButton == ButtonState.Pressed)
                         {
-                            test = i;
+                            SelectedItemID = i;
                             ResourseClick();
                         }
                     }
@@ -237,7 +276,7 @@ namespace Project7
                     if (Mouse.GetState().LeftButton == ButtonState.Pressed)
                     {
                         ButtonPressed = true;
-                        UpButtonClick(test);
+                        UpButtonClick(SelectedItemID);
                     }
                 }
                 if (Down.SpriteRectangle.Contains(Mouse.GetState().Position) && ButtonPressed == false)
@@ -245,22 +284,73 @@ namespace Project7
                     if (Mouse.GetState().LeftButton == ButtonState.Pressed)
                     {
                         ButtonPressed = true;
-                        DownButtonClick(test);
+                        DownButtonClick(SelectedItemID);
                     }
                 }
             }
+
+            if(GlobalStatus==1)
+            {
+                if (Mouse.GetState().LeftButton == ButtonState.Released)
+                    ButtonPressed = false;
+
+                if (No.SpriteRectangle.Contains(Mouse.GetState().Position))
+                {
+                    if (Mouse.GetState().LeftButton == ButtonState.Pressed && ButtonPressed == false)
+                        NoButtonClick();
+                }
+
+                for (int i = 0; i < Building.Count; i++)
+                {
+                    if (Building[i].SpriteRectangle.Contains(Mouse.GetState().Position))
+                    {
+                        ButtonSelected = true;
+                        Selection2.SpriteRectangle = new Rectangle(Building[i].SpriteRectangle.X - 5, Building[i].SpriteRectangle.Y - 5, 90, 90);
+                        if (Mouse.GetState().LeftButton == ButtonState.Pressed)
+                        {
+                            SelectedBuildingID = i;
+                            BuildingClick();
+                        }
+                    }
+                }
+            }
+
+            if(GlobalStatus == 10)
+            {
+                if (Mouse.GetState().LeftButton == ButtonState.Released)
+                    ButtonPressed = false;
+
+                if (No.SpriteRectangle.Contains(Mouse.GetState().Position))
+                {
+                    if (Mouse.GetState().LeftButton == ButtonState.Pressed)
+                    {
+                        ButtonPressed = true;
+                        NoButtonClick();
+                    }
+                }
+                if (Yes.SpriteRectangle.Contains(Mouse.GetState().Position))
+                {
+                    if (Mouse.GetState().LeftButton == ButtonState.Pressed)
+                        YesButtonClick();
+                }
+            }
+
             WaterfallAnimation(gameTime);
             DayCounter(gameTime);
 
             base.Update(gameTime);
         }
-       
+        private void BuildingClick()
+        {
+            GlobalStatus = 10;
+            ButtonSelected = false;
+        }
         private void UpButtonClick(int test2)
         {
-            if (IntUnsafeGold >= Resourse[test].ResourseType.price)
+            if (IntUnsafeGold >= Resourse[SelectedItemID].ResourseType.price)
             {
                 IntUnsafeRes++;
-                IntUnsafeGold -= Resourse[test].ResourseType.price;
+                IntUnsafeGold -= Resourse[SelectedItemID].ResourseType.price;
             }
         }
         private void DownButtonClick(int test2)
@@ -268,22 +358,51 @@ namespace Project7
             if (IntUnsafeRes >= 1)
             {
                 IntUnsafeRes--;
-                IntUnsafeGold += Resourse[test].ResourseType.price;
+                IntUnsafeGold += Resourse[SelectedItemID].ResourseType.price;
             }
         }
         private void YesButtonClick()
         {
-            Resourse[7].ResourseType.count = IntUnsafeGold;
-            Resourse[test].ResourseType.count = IntUnsafeRes;
-            GlobalStatus = 3;
+            if (GlobalStatus == 30)
+            {
+                Resourse[7].ResourseType.count = IntUnsafeGold;
+                Resourse[SelectedItemID].ResourseType.count = IntUnsafeRes;
+                GlobalStatus = 3;
+            }
+            else if(GlobalStatus ==10)
+            {
+                int count = 0;
+                for (int i = 0; i < Building[SelectedBuildingID].BuildingType.BuildingPrice[Building[SelectedBuildingID].BuildingType.currentLevel].Count; i++)
+                {
+
+
+                    if (Resourse[(int)Building[SelectedBuildingID].BuildingType.BuildingPrice[Building[SelectedBuildingID].BuildingType.currentLevel].ElementAt(i).Key].ResourseType.count >= Building[SelectedBuildingID].BuildingType.BuildingPrice[Building[SelectedBuildingID].BuildingType.currentLevel].ElementAt(i).Value)
+                        count++;
+
+                }
+                if (count == Building[SelectedBuildingID].BuildingType.BuildingPrice[Building[SelectedBuildingID].BuildingType.currentLevel].Count)
+                {
+                    for (int i = 0; i < Building[SelectedBuildingID].BuildingType.BuildingPrice[Building[SelectedBuildingID].BuildingType.currentLevel].Count; i++)
+                    {
+                        Resourse[(int)Building[SelectedBuildingID].BuildingType.BuildingPrice[Building[SelectedBuildingID].BuildingType.currentLevel].ElementAt(i).Key].ResourseType.count = Resourse[(int)Building[SelectedBuildingID].BuildingType.BuildingPrice[Building[SelectedBuildingID].BuildingType.currentLevel].ElementAt(i).Key].ResourseType.count - Building[SelectedBuildingID].BuildingType.BuildingPrice[Building[SelectedBuildingID].BuildingType.currentLevel].ElementAt(i).Value;
+
+                    }
+                    GlobalStatus = 1;
+                    Building[SelectedBuildingID].BuildingType.currentLevel++;
+                }
+            }
             ButtonSelected = false;
         }
         private void NoButtonClick()
         {
             if (GlobalStatus == 30)
                 GlobalStatus = 3;
-            else
-            GlobalStatus = 0;
+            else if (GlobalStatus == 3)
+                GlobalStatus = 0;
+            else if (GlobalStatus == 10)
+                GlobalStatus = 1;
+            else if (GlobalStatus == 1)
+                GlobalStatus = 0;
 
             ButtonSelected = false;
         }
@@ -302,10 +421,11 @@ namespace Project7
             GlobalStatus = 3;
             ButtonSelected = false;
         }
-        int test = 0;
+        int SelectedItemID = 0;
+        int SelectedBuildingID = 0;
         private void ResourseClick()
         {
-            IntUnsafeRes= Resourse[test].ResourseType.count;
+            IntUnsafeRes= Resourse[SelectedItemID].ResourseType.count;
             IntUnsafeGold = Resourse[7].ResourseType.count;
 
             GlobalStatus = 30;
@@ -342,9 +462,7 @@ namespace Project7
             if (GlobalStatus == 0)
             {
                 _spriteBatch.Draw(wf,new Vector2(436,240),Color.White);
-                _spriteBatch.Draw(tx, new Vector2(280, 510), Color.White);
-                _spriteBatch.Draw(col, new Vector2(335, 450), Color.White);
-
+               
                 _spriteBatch.Draw(BuildMenu.Texture, new Vector2(BuildMenu.SpriteRectangle.X, BuildMenu.SpriteRectangle.Y), Color.White);
                 _spriteBatch.Draw(WorkMenu.Texture, new Vector2(WorkMenu.SpriteRectangle.X, WorkMenu.SpriteRectangle.Y), Color.White);
                 _spriteBatch.Draw(ResourseMenu.Texture, new Vector2(ResourseMenu.SpriteRectangle.X, ResourseMenu.SpriteRectangle.Y), Color.White);
@@ -371,7 +489,7 @@ namespace Project7
 
             if(GlobalStatus == 30)
             {
-                _spriteBatch.Draw(Resourse[test].Texture, new Vector2(220,270), Color.White);
+                _spriteBatch.Draw(Resourse[SelectedItemID].Texture, new Vector2(220,270), Color.White);
                 _spriteBatch.Draw(Resourse[7].Texture, new Vector2(220, 480), Color.White);
                 _spriteBatch.Draw(No.Texture, new Vector2(No.SpriteRectangle.X, No.SpriteRectangle.Y), Color.White);
                 _spriteBatch.Draw(Yes.Texture, new Vector2(Yes.SpriteRectangle.X, Yes.SpriteRectangle.Y), Color.White);
@@ -381,7 +499,58 @@ namespace Project7
                 _spriteBatch.DrawString(UnsafeRes, IntUnsafeRes.ToString(), new Vector2(320,270), Color.White);
                 _spriteBatch.DrawString(UnsafeGold, IntUnsafeGold.ToString(), new Vector2(320,480), Color.White);
             }
-           
+
+            if(GlobalStatus == 1)
+            {
+                _spriteBatch.Draw(No.Texture, new Vector2(No.SpriteRectangle.X, No.SpriteRectangle.Y), Color.White);
+
+                if (ButtonSelected == true)
+                    _spriteBatch.Draw(Selection2.Texture, new Vector2(Selection2.SpriteRectangle.X, Selection2.SpriteRectangle.Y), Color.White);
+                for (int i = 0; i < Building.Count; i++)
+                {
+                    _spriteBatch.Draw(Building[i].Icon[0], new Vector2(Building[i].SpriteRectangle.X, Building[i].SpriteRectangle.Y), Color.White);
+                    
+                }
+            }
+
+            if(GlobalStatus==10)
+            {
+                _spriteBatch.Draw(No.Texture, new Vector2(No.SpriteRectangle.X, No.SpriteRectangle.Y), Color.White);
+                _spriteBatch.Draw(Yes.Texture, new Vector2(Yes.SpriteRectangle.X, Yes.SpriteRectangle.Y), Color.White);
+
+                if(Building[SelectedBuildingID].BuildingType.currentLevel == 0)
+                {
+                    _spriteBatch.Draw(Selection2.Texture, new Vector2(195,195), Color.White);
+                    _spriteBatch.Draw(Building[SelectedBuildingID].Icon[1], new Vector2(200, 200), Color.White);
+                    _spriteBatch.Draw(Building[SelectedBuildingID].Icon[1], new Vector2(300, 200), Color.White);
+                    _spriteBatch.Draw(Building[SelectedBuildingID].Icon[1], new Vector2(400, 200), Color.White);
+                }
+                else if (Building[SelectedBuildingID].BuildingType.currentLevel == 1)
+                {
+                    _spriteBatch.Draw(Selection2.Texture, new Vector2(295, 195), Color.White);
+                    _spriteBatch.Draw(Building[SelectedBuildingID].Icon[0], new Vector2(200, 200), Color.White);
+                    _spriteBatch.Draw(Building[SelectedBuildingID].Icon[1], new Vector2(300, 200), Color.White);
+                    _spriteBatch.Draw(Building[SelectedBuildingID].Icon[1], new Vector2(400, 200), Color.White);
+                }
+                else if (Building[SelectedBuildingID].BuildingType.currentLevel == 2)
+                {
+                    _spriteBatch.Draw(Selection2.Texture, new Vector2(395, 195), Color.White);
+                    _spriteBatch.Draw(Building[SelectedBuildingID].Icon[0], new Vector2(200, 200), Color.White);
+                    _spriteBatch.Draw(Building[SelectedBuildingID].Icon[0], new Vector2(300, 200), Color.White);
+                    _spriteBatch.Draw(Building[SelectedBuildingID].Icon[1], new Vector2(400, 200), Color.White);
+                }
+
+                for (int i = 0; i < Building[SelectedBuildingID].BuildingType.BuildingPrice[Building[SelectedBuildingID].BuildingType.currentLevel].Count; i++)
+                {
+                   _spriteBatch.Draw(Resourse[(int)Building[SelectedBuildingID].BuildingType.BuildingPrice[Building[SelectedBuildingID].BuildingType.currentLevel].ElementAt(i).Key].Texture , new Vector2(200 , 300 + i * 100), Color.White);
+
+                    _spriteBatch.DrawString(font,$"{Resourse[(int)Building[SelectedBuildingID].BuildingType.BuildingPrice[Building[SelectedBuildingID].BuildingType.currentLevel].ElementAt(i).Key].ResourseType.count}" +
+                        $"/{Building[SelectedBuildingID].BuildingType.BuildingPrice[Building[SelectedBuildingID].BuildingType.currentLevel].ElementAt(i).Value}  ", new Vector2(320, 300+i*100), 
+                        Resourse[(int)Building[SelectedBuildingID].BuildingType.BuildingPrice[Building[SelectedBuildingID].BuildingType.currentLevel].ElementAt(i).Key].ResourseType.count >= Building[SelectedBuildingID].BuildingType.BuildingPrice[Building[SelectedBuildingID].BuildingType.currentLevel].ElementAt(i).Value ? Color.Green:Color.Red);
+
+                }
+
+            }
 
 
             _spriteBatch.End();
